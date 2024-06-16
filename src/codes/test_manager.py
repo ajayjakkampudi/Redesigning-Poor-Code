33e358be9ad7fd4f49b2564717eff429f7c03dc3
Ajayj2024 <ajayjakkampudi@snive.io>
2024-06-16 23:56:08 +0530

    unit testing for add functionality of manager class

--- src/codes/test_manager.py
+++ src/codes/test_manager.py
@@ -2,19 +2,29 @@ import sys, os
 sys.path.append(os.path.join(os.getcwd(),'src'))
 
 import unittest
-from codes.manager import BookManager
+from codes.manager import BookManager, UserManager
 from codes.database import DatabaseConfig
 
 # Intiates the unit testing
 class TestManage(unittest.TestCase):
     book_manager = BookManager()
+    user_manager = UserManager()
     # Testing the add functinality
-    def test_add(self):
+    def test_book_add(self):
         data = dict(zip(DatabaseConfig.book_manage_cols,['A','a',1,'yes']))
         data = {k:v for k,v in data.items()}
         
         # adding data to database
         added_data = dict(self.book_manager.add(data).iloc[-1])
+        # tests the the added is appended or not
+        self.assertEqual(added_data, data)
+        
+    def test_user_add(self):
+        data = dict(zip(DatabaseConfig.user_manage_cols,['A',11]))
+        data = {k:v for k,v in data.items()}
+        
+        # adding data to database
+        added_data = dict(self.user_manager.add(data).iloc[-1])
         # tests the the added is appended or not
         self.assertEqual(added_data, data)
         
